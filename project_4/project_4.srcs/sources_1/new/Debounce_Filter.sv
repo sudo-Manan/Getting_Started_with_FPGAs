4c4b5ed5ae920ebf733ea19c5b49e27a95a178bb
sudo-Manan <called.manan.i.am@gmail.com>
2026-09-12 23:53:36 +0530

    fix: added paranthesis for better understanding and to avoid errors

--- project_4/project_4.srcs/sources_1/new/Debounce_Filter.sv
+++ project_4/project_4.srcs/sources_1/new/Debounce_Filter.sv
@@ -20,17 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module Debounce_Filter #(parameter DEBOUNCE_LIMIT=20)(
+module Debounce_Filter #(parameter DEBOUNCE_LIMIT = 20)(
     input logic in_clk, in_bouncy,
     output logic out_debounced
 );
     
-    logic [$clog2(DEBOUNCE_LIMIT)-1:0] r_count = 0;
+    logic [($clog2(DEBOUNCE_LIMIT))-1:0] r_count = 0;
     logic r_state = 1'b0;
     
     always_ff @(posedge in_clk) begin
-        if (in_bouncy != r_state && r_count < DEBOUNCE_LIMIT-1) r_count <= r_count + 1;
-        else if (r_count == DEBOUNCE_LIMIT - 1) begin
+        if ((in_bouncy != r_state) && (r_count < DEBOUNCE_LIMIT-1)) r_count <= r_count + 1;
+        else if (r_count == (DEBOUNCE_LIMIT - 1)) begin
             r_state <= in_bouncy;
             r_count <= 0;
         end
